e3dce5fb769f821212be7a642de14326bdc5024e
Grootguo <genyuan.guo@gmail.com>
2024-06-14 20:45:28 +0800

    add item service code

--- internal/logic/itemservice/createiteminfologic.go
+++ internal/logic/itemservice/createiteminfologic.go
@@ -2,11 +2,13 @@ package itemservicelogic
 
 import (
 	"context"
-
-	"wechat-merchant-go/internal/svc"
-	"wechat-merchant-go/pb/sku"
+	"errors"
 
 	"github.com/zeromicro/go-zero/core/logx"
+
+	"wechat-merchant-go/internal/dao/model"
+	"wechat-merchant-go/internal/svc"
+	"wechat-merchant-go/pb/sku"
 )
 
 type CreateItemInfoLogic struct {
@@ -24,7 +26,37 @@ func NewCreateItemInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Cr
 }
 
 func (l *CreateItemInfoLogic) CreateItemInfo(in *sku.CreateOrUpdateItemInfoReq) (*sku.CommonRsp, error) {
-	// todo: add your logic here and delete this line
+	if in.GetShopId() == 0 || in.GetItemId() == 0 || in.GetItemName() == "" || in.GetCategoryId() == 0 || in.GetCategoryNam() == "" {
+		return nil, errors.New("params error")
+	}
 
-	return &sku.CommonRsp{}, nil
+	shopTab := l.svcCtx.Use.ShopInfoTab
+	shopData, err := shopTab.WithContext(l.ctx).Where(shopTab.ShopID.Eq(in.GetShopId())).First()
+	if err != nil {
+		l.Errorf("CreateItemInfo error:%+v", err)
+		return nil, err
+	}
+	if shopData == nil {
+		return nil, errors.New("shop not found")
+	}
+
+	// todo category id 校验, 忘记加这张表了
+
+	u := l.svcCtx.Use.ItemInfoTab
+	err = u.WithContext(l.ctx).Create(&model.ItemInfoTab{
+		ItemID:       in.GetItemId(),
+		ItemName:     in.GetItemName(),
+		CategoryID:   in.GetCategoryId(),
+		CategoryName: in.GetCategoryNam(),
+		ShopID:       in.GetShopId(),
+	})
+	if err != nil {
+		l.Errorf("CreateItemInfo error:%+v", err)
+		return nil, err
+	}
+
+	return &sku.CommonRsp{
+		Code: 0,
+		Msg:  "success",
+	}, nil
 }
